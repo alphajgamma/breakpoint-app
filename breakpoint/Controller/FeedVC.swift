c567bb72c280986b35a9906c91b27ada6ed4041a
Andrew Greenough <andygreenough@gmail.com>
2017-09-05 17:43:40 +0100

    Converting UIDs into Emails and Reversing the Order of a TableView

--- breakpoint/Controller/FeedVC.swift
+++ breakpoint/Controller/FeedVC.swift
@@ -25,11 +25,10 @@ class FeedVC: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         DataService.instance.getAllFeedMessages { (returnedMessagesArray) in
-            self.messageArray = returnedMessagesArray
+            self.messageArray = returnedMessagesArray.reversed()
             self.tableView.reloadData()
         }
     }
-    
 }
 
 extension FeedVC: UITableViewDelegate, UITableViewDataSource {
@@ -46,7 +45,9 @@ extension FeedVC: UITableViewDelegate, UITableViewDataSource {
         let image = UIImage(named: "defaultProfileImage")
         let message = messageArray[indexPath.row]
         
-        cell.configureCell(profileImage: image!, email: message.senderId, content: message.content)
+        DataService.instance.getUsername(forUID: message.senderId) { (returnedUsername) in
+            cell.configureCell(profileImage: image!, email: returnedUsername, content: message.content)
+        }
         return cell
     }
 }
